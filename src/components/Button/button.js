e6f9b6140091587fdc59babacb48cf482b68f34f
chang-mac <835702580@qq.com>
2017-10-23 17:28:19 +0800

    add classnames

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import classnames from 'classnames'
 import styles from './style'
 
 console.log(styles)
+
+// other falsy values are just ignored
+const classes = classnames(null, false, 'bar', undefined, 0, 1, { baz: null }, ''); // => 'bar 1'
+console.log(classes)   // undefined is ignored
 
 class Button extends React.Component {
     render() {
